c7e0f4df0dea0ee3a6bc77b7a8221f67ac73606c
Felix Schwart <util.fs@tuta.com>
2024-09-08 21:12:42 +0200

    deleting unnecessary ccRouting (multithreading at that level is too inefficient)

--- src/concurrentRouting.hpp
+++
@@ -1,9 +0,0 @@
-#include <map>
-#include <set>
-#include "channel/channel.hpp"
-#include "block.hpp"
-#include "findResult.hpp"
-
-findResult findPinWithThreads(std::set<unsigned char> const &tracksToCheck, std::map<unsigned char, std::map<channelID, unsigned char>> &channelToIndexMaps, std::map<unsigned char, std::map<unsigned char, std::set<channelID>>> &indexToChannelsMaps, unsigned char arraySize,
-                             unsigned char channelWidth, std::map<channelID, channelInfo> const &channelInformation, std::map<channelID, std::set<std::string>> const &relevantChannels,
-                             std::set<channelID> const &doublyRelevantChannels, std::map<std::string, std::shared_ptr<block>> const &blocks);
